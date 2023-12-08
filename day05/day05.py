7079c14aa69b53389d2a9b3a42e47cee652e7d42
VastInfiniteNet <screwthisusernameprocess@gmail.com>
2023-12-07 23:22:06 -0500

    .

--- day05/day05.py
+++ day05/day05.py
@@ -25,42 +25,5 @@ def part1():
    return min(seeds)
 
 
-def mapPair(pair, mapPoints): # check where current seed value should be mapped to
-      start, end = pair
-      best = (100 ** 10, 100 ** 10)
-      for i in range(len(mapPoints) - 1):
-         left, right = mapPoints[i], mapPoints[i + 1]
-         if end < left[0] or start > right[0]:
-            continue
-         value = min(right[0], end) + right[1]
-         if value < best[1]:
-            best = (min(right[0], end), value)
-      return best
-   
-def mergeMaps(old, new):
-   updatedPoints = []
-   print("OLD:", old, "\nNEW:", new, '\n')
-   while old and new:
-      if new[0][0] < old[0][0]: # new first
-         point = new.pop(0)
-         other = old
-         if len(other) % 2 == 1: # point within band
-            point[1] += other[0][1]
-      elif new[0][0] > old[0][0]: # old first
-         point = old.pop(0)
-         other = new
-         if len(other) % 2 == 1: # point within band
-            point[1] += other[0][1]
-      else: # both
-         point = old.pop(0)
-         point[1] = new.pop(0)[1]
-      updatedPoints.append(point)
-   if old:
-      updatedPoints += old
-   else:
-      updatedPoints += new
-   
-   return updatedPoints
-
 
 print(part1())
