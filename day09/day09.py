009d3b4b842af53d67c515d826c25d46e506e49a
VastInfiniteNet <screwthisusernameprocess@gmail.com>
2023-12-14 19:56:59 -0500

    cleanup

--- day09/day09.py
+++ day09/day09.py
@@ -1,20 +1,15 @@
 def part1():
    total = 0
-   readings = []   
-   f = open("{}/input".format(__file__[-8:-3]))
-   for line in f:
-      readings.append(list(map(int, line.strip().split())))
-   f.close()
+   with open("{}/input".format(__file__[-8:-3])) as f:
+      readings = [list(map(int, line.strip().split())) for line in f] 
 
    for r in readings:
       rows = [r]
-      while set(rows[-1]) != set([0] * len(rows[-1])):
+      while set(rows[-1]) != set([0]):
          rows.append([rows[-1][i] - rows[-1][i-1] for i in range(1, len(rows[-1]))])
       
-      print(rows)
       while len(rows) > 1: # predict readings changes
          rows[-2].append(rows[-1][-1] + rows[-2][-1])
-         print(rows[-2])
          rows.pop()
       total += rows[0][-1]
    
@@ -22,21 +17,16 @@ def part1():
 
 def part2():
    total = 0
-   readings = []   
-   f = open("{}/input".format(__file__[-8:-3]))
-   for line in f:
-      readings.append(list(map(int, line.strip().split())))
-   f.close()
+   with open("{}/input".format(__file__[-8:-3])) as f:
+      readings = [list(map(int, line.strip().split())) for line in f] 
 
    for r in readings:
       rows = [r]
-      while set(rows[-1]) != set([0] * len(rows[-1])):
+      while set(rows[-1]) != set([0]):
          rows.append([rows[-1][i] - rows[-1][i-1] for i in range(1, len(rows[-1]))])
       
-      print(rows)
       while len(rows) > 1: # predict readings changes
          rows[-2] = [rows[-2][0] - rows[-1][0]] + rows[-2]
-         print(rows[-2])
          rows.pop()
       total += rows[0][0]
 
